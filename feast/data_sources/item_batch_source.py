d21a495a4aff5d50b17d24386af501e6dbea6afb
Encore7 <amankumar5150@gmail.com>
2025-11-28 12:18:56 +0530

    feast update

--- feast/data_sources/item_batch_source.py
+++
@@ -1,8 +0,0 @@
-from feast import FileSource
-
-item_batch_source = FileSource(
-    name="item_events_source",
-    path="s3://recgym-raw/events/*.parquet",
-    timestamp_field="ts",
-    event_timestamp_column="ts",
-)
--- feast/data_sources/user_batch_source.py
+++
@@ -1,9 +0,0 @@
-from feast import FileSource
-from datetime import timedelta
-
-user_batch_source = FileSource(
-    name="user_events_source",
-    path="s3://recgym-raw/events/*.parquet",
-    timestamp_field="ts",
-    event_timestamp_column="ts",
-)
